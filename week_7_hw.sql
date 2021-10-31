371a89f4c7171172d023aaf2551db2934a5d5e2e
NikOdeemus <nicolejonmiller@gmail.com>
2021-10-31 17:48:27 -0500

    adding datacamp pngs

---
+++ week_7_hw.sql
@@ -0,0 +1,31 @@
+-- Create a new column called “status” in the rental table that uses a case statement to indicate if a film was returned late, early, or on time. 
+
+
+-- Show the total payment amounts for people who live in Kansas City or Saint Louis. 
+
+
+-- How many films are in each category? Why do you think there is a table for category and a table for film category?
+
+
+-- Show a roster for the staff that includes their email, address, city, and country (not ids)
+
+
+-- Show the film_id, title, and length for the movies that were returned from May 15 to 31, 2005
+
+
+-- Write a subquery to show which movies are rented below the average price for all movies. 
+
+
+-- Write a join statement to show which movies are rented below the average price for all movies.
+
+
+-- Perform an explain plan on 6 and 7, and describe what you’re seeing and important ways they differ.
+
+
+-- With a window function, write a query that shows the film, its duration, and what percentile the duration fits into. This may help https://mode.com/sql-tutorial/sql-window-functions/#rank-and-dense_rank 
+
+
+-- In under 100 words, explain what the difference is between set-based and procedural programming. Be sure to specify which sql and python are. 
+
+
+-- Bonus: Find the relationship that is wrong in the data model. Explain why it’s wrong. 
